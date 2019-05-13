19dfd906bae1f16a7cec4d3ff22418aff17a56ad
Rakesh Mistry <rakesh@qualica.com>
2019-05-13 21:49:59 +0200

    Chapter2 exercise 1

--- go-lang/chapter2/hello-world.go
+++ go-lang/chapter2/hello-world.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 import "bufio"
 import "os"
+import "strings"
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println(reader)
+	fmt.Println("What is your name?")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("Hello, %s, nice to meet you.", strings.TrimSuffix(text, "\n"))
+	reader.ReadString('\n')
 }
