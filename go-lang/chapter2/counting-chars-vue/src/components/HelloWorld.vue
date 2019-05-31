815dd45d134819a73fd61ab95586f7eee9e86dc1
Rakesh Mistry <rakesh.mistry@gmail.com>
2019-05-31 10:33:07 +0200

    Made the html look nice

--- go-lang/chapter2/counting-chars-vue/src/components/HelloWorld.vue
+++ go-lang/chapter2/counting-chars-vue/src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="hello">
-        <p>Please enter some text</p>
-        <input id="inp" placeholder="Enter something" @input="countMyChars">
-        <p>Count of characters : {{returnCount()}}</p>
-    </div>
+    <div>
+        <div class="left-pad"/>
+            <div class="hello">
+                <p>Please enter some text</p>
+                <input id="inp" placeholder="Enter something" @input="countMyChars">
+                <div class="result">
+                    <p>Count of characters : {{returnCount()}}</p>
+                </div>
+            </div>
+
+            <div class="right-pad"/>
+            </div>
 </template>
 
 <script>
@@ -50,5 +57,31 @@ li {
 }
 a {
     color: #42b983;
+}
+.hello {
+    margin: 0% 40% 0% 40%;
+    background-color: LightSteelBlue;
+    border-radius: 5px;
+    box-shadow: 1px 1px 15px 1px Gray;
+}
+input {
+    padding: 10px;
+    font: 2em;
+}
+p {
+    padding: 30px 5px 5px 10px;
+    font: 15px ;
+    font-weight: bold;
+}
+.left-pad {
+    margin: 5% 30 5% 30%;
+}
+.right-pad {
+    margin: 5% 30 5% 30%;
+}
+.result {
+    background-color: MidnightBlue;
+    color: white;
+    border-radius: 5px;
 }
 </style>
