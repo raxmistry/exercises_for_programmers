7a1379f42d26ff00530d1f69af2842d97f131290
Rakesh Mistry <rakesh.mistry@gmail.com>
2019-05-31 09:59:01 +0200

    Got vue component to update dynaimcally

--- go-lang/chapter2/counting-chars-vue/src/components/HelloWorld.vue
+++ go-lang/chapter2/counting-chars-vue/src/components/HelloWorld.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="hello">
-      <input v-model="message" placeholder="Enter something" @input="countMyChars">
-  </div>
+    <div class="hello">
+        <p>Please enter some text</p>
+        <input id="inp" placeholder="Enter something" @input="countMyChars">
+        <p>Count of characters : {{returnCount()}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-  },
+    name: 'HelloWorld',
+    props: {
+
+    },
+    data: function() {
+        return {
+            count: "0"
+        }
+    },
     methods: {
         countMyChars : function(event) {
-            var data = event.data
+            var data = document.getElementById("inp").value 
             console.log(data)
             var url = "http://localhost:8080/" + data
             console.log(url)
             this.axios.get(url).then( (response) => {
                 console.log(response.data)
+                this.count = response.data;
             });
+        },
+        returnCount : function( ) {
+            return this.count;
         }
     }
 }
@@ -26,17 +38,17 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
